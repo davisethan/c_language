16f66e3e84955aec14f78543205520709fd54973
davisethan <davisethan.work@gmail.com>
2021-02-18 11:57:36 -0800

    Ex 1 - 3 added

---
+++ ex1.c
@@ -0,0 +1,11 @@
+# include <stdio.h>
+
+/* A comment */
+int main(int argc, char *argv[]) {
+    int distance = 100;
+    
+    // Another comment
+    printf("You are %d miles away.\n", distance);
+    
+    return 0;
+}
